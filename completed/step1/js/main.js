7642a1c9c50a4b46a097bb3c2a296bb400ee0667
Sam Dutton <dutton@google.com>
2015-08-14 11:12:48 +0100

    Added up to Step 6

--- completed/step1/js/main.js
+++ completed/step1/js/main.js
@@ -2,10 +2,12 @@
 
 if ('serviceWorker' in navigator) {
   console.log('Service Worker is supported');
-  navigator.serviceWorker.register('sw.js').then(function(reg) {
-    console.log(':^)', reg);
-    // TODO: Step 4
+  navigator.serviceWorker.register('sw.js').then(function() {
+    return navigator.serviceWorker.ready;
+  }).then(function(reg) {
+    console.log('Service Worker is ready :^)', reg);
+      // TODO: Step 4
   }).catch(function(error) {
-    console.log(':^(', error);
+    console.log('Service Worker error :^(', error);
   });
 }
